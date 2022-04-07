b4a91c79a1a149586ff2c34bea49cf6c7dd09e2f
Maximilian Stocker <max.stocker@gmx.de>
2022-04-07 14:23:00 +0200

    letzte aufgabe fehlt

--- datenbank.h
+++ datenbank.h
@@ -1,23 +1,33 @@
 #pragma once
 #define DB_LIMIT 10
-#include "kennzeichen.h"
 #include <string>
+
+#include "kennzeichen.h"
 namespace db {
-struct datenbank {
+struct datenbank
+{
   std::string name;
-  kfz::kennzeichen *schilder[DB_LIMIT];
+  kfz::kennzeichen* schilder[DB_LIMIT];
   int eintraege = 0;
 };
 typedef struct datenbank datenbank;
-bool einfuegen(datenbank *db, kfz::kennzeichen *schild) { 
-	if (db->eintraege < DB_LIMIT) {
-		std::cout << "Zu viele eintraege!" << std::endl;
-		return false;
-	}
-	return true;
+bool
+einfuegen(datenbank* db, kfz::kennzeichen* schild)
+{
+  db->schilder[db->eintraege] = schild; //da eintrage gleich 0 ist können wir diese als Variable gleich benutzen
+  db->eintraege++; //bei erfolgreichem einfügen erhöhen wir gleichzeitig diese Variable
+
+  if (db->eintraege >= DB_LIMIT) { //dies hat den Vorteil, dass wir ohne Hilfsvariablen gleich sehen können ob wir in den Limiter laufen oder nicht
+    std::cout << "Datenlimit erreicht. breche ab!" << std::endl;
+    return false;
+  }
+  return true;
 }
-std::string ausgabe(const datenbank &db) {
-	std::string text_ausgabe {};
-	return text_ausgabe;
+std::string
+ausgabe(const datenbank& db)
+{
+  std::string r_datenbank = db.name;
+  r_datenbank += "\n";
+  return r_datenbank;
 }
 } // namespace db
--- main.cpp
+++ main.cpp
@@ -1,29 +1,37 @@
 #include <iostream>
-#include "kennzeichen.h"
+
 #include "datenbank.h"
-void schnapszahlTest() { 
-	kfz::kennzeichen k;
-	k.ort = 'N';
-	k.buchstaben = "Nu";
-	k.zahl = 333;
-
-	kfz::kennzeichen* kptr;
-	kptr = &k;
-
-	std::cout << "Ergbenis, des Schnapszahltest" << kfz::istSchnapszahl(kptr) << std::endl;
-
-}
-void einlesenTest() {
-	kfz::kennzeichen* kennzeichen_zeiger;
-	kennzeichen_zeiger->buchstaben = kfz::einlesenBuchstaben();
-	kennzeichen_zeiger->ort = kfz::einlesenOrt();
-	kennzeichen_zeiger->zahl= kfz::einlesenZahl();
-
-	kfz::ausgabe(kennzeichen_zeiger);
-	kfz::schildTest(kennzeichen_zeiger);
-}
-void datenbankTest() {}
-int main()
+#include "kennzeichen.h"
+void
+schnapszahlTest()
 {
-return 0;
+  kfz::kennzeichen k;
+  k.ort = 'N';
+  k.buchstaben = "Nu";
+  k.zahl = 333;
+
+  kfz::kennzeichen* kptr;
+  kptr = &k;
+
+  std::cout << "Ergbenis, des Schnapszahltest" << kfz::istSchnapszahl(kptr)
+            << std::endl;
+}
+void
+einlesenTest()
+{
+  kfz::kennzeichen* kennzeichen_zeiger;
+  kennzeichen_zeiger->buchstaben = kfz::einlesenBuchstaben();
+  kennzeichen_zeiger->ort = kfz::einlesenOrt();
+  kennzeichen_zeiger->zahl = kfz::einlesenZahl();
+
+  kfz::ausgabe(kennzeichen_zeiger);
+  kfz::schildTest(kennzeichen_zeiger);
+}
+void
+datenbankTest()
+{}
+int
+main()
+{
+  return 0;
 }
